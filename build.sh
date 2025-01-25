1adce1466942088a892948901b403b124992d9a0
ramprasad1090 <dramprasad5261@gmail.com>
2025-01-25 16:11:41 +0000

    bash script

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+IMAGE_NAME="ramprasad1090/react-app-dev"
+
+docker build -t $IMAGE_NAME .
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+IMAGE_NAME="ramprasad1090/react-app-dev"
+
+docker run -d -p 80:80 $IMAGE_NAME
